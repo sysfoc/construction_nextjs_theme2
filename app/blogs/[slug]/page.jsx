f2e41dc315bf70bd03304046cc07041a248c57ad
Rehan Ahmad <rehan048686@gmail.com>
2025-12-03 15:49:49 +0500

    blog fix

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -1,8 +1,14 @@
 import BlogDetail from "@/app/components/blog/BlogDetail";
 
 export async function generateMetadata({ params }) {
-  const res = await fetch(`${process.env.BASE_URL}/api/blog/${params.slug}`);
+  const BASE =
+    process.env.NODE_ENV === "development"
+      ? process.env.BASE_URL
+      : process.env.NEXT_PUBLIC_BASE_URL;
+
+  const res = await fetch(`${BASE}/api/blog/${params.slug}`);
   const data = await res.json();
+
   if (!data.blog) {
     return {
       title: "Blog not found",
@@ -17,8 +23,14 @@ export async function generateMetadata({ params }) {
 }
 
 const BlogPage = async ({ params }) => {
-  const res = await fetch(`${process.env.BASE_URL}/api/blog/${params.slug}`);
+  const BASE =
+    process.env.NODE_ENV === "development"
+      ? process.env.BASE_URL
+      : process.env.NEXT_PUBLIC_BASE_URL;
+
+  const res = await fetch(`${BASE}/api/blog/${params.slug}`);
   const data = await res.json();
+
   return <BlogDetail blog={data.blog} />;
 };
 
